ab1bd3196c6edc1350ec053bb73c5b1294ef9da5
ksomemo <kntsmy@hotmail.co.jp>
2012-10-23 23:00:42 +0900

    整数の配列にある数が含まれているかを仮実装した。

---
+++ src/lecture1/Search.php
@@ -0,0 +1,9 @@
+<?php
+
+class Search
+{
+    public static function contains($v, $vs)
+    {
+        return true;
+    }
+}
---
+++ test/lecture1/SearchTest.php
@@ -0,0 +1,19 @@
+<?php
+require_once __DIR__.'/../../src/lecture1/Search.php';
+
+class SearchTest extends PHPUnit_Framework_Testcase
+{
+    public function test100ContainsIn1to10000000RangeArray()
+    {
+        ini_set('memory_limit', '1024M');
+
+        $vs = range(1, 10000000);
+
+        $v = 100;
+
+        $condition = Search::contains($v, $vs);
+
+        $this->assertTrue($condition);
+    }
+
+}
