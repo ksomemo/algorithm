8bf66626aa1f7fe148e51565158dffa1d0eccd0d
ksomemo <kntsmy@hotmail.co.jp>
2012-10-28 18:36:18 +0900

    木の作成をsetUpで行うようにした。

--- test/lecture2/TreeTest.php
+++ test/lecture2/TreeTest.php
@@ -4,62 +4,61 @@ require_once __DIR__.'/../../src/lecture2/TreeNode.php';
 
 class TreeTest extends PHPUnit_Framework_TestCase
 {
+    private $tree;
+
+    public function setUp()
+    {
+        $this->tree = new Tree();
+    }
+
     public function testTreeRootNodeIsNullAtJustTreeMaked()
     {
-        $tree = new Tree();
-
         $expected = null;
 
-        $this->assertSame($expected, $tree->getRoot());
+        $this->assertSame($expected, $this->tree->getRoot());
     }
 
     public function testReplaceRootWithTreeNode()
     {
-        $tree     = new Tree();
-
         $newRoot = new TreeNode(1);
-        $tree->replace(null, null, $newRoot);
-        $this->assertSame(1, $tree->getRoot()->getValue());
+        $this->tree->replace(null, null, $newRoot);
+        $this->assertSame(1, $this->tree->getRoot()->getValue());
 
         $newLeft = new TreeNode(2);
-        $tree->replace($tree->getRoot(), null, $newLeft);
-        $this->assertSame(2, $tree->getRoot()->getLeft()->getValue());
+        $this->tree->replace($this->tree->getRoot(), null, $newLeft);
+        $this->assertSame(2, $this->tree->getRoot()->getLeft()->getValue());
 
         $newRight = new TreeNode(3);
-        $tree->replace($tree->getRoot(), null, $newRight);
-        $this->assertSame(3, $tree->getRoot()->getRight()->getValue());
+        $this->tree->replace($this->tree->getRoot(), null, $newRight);
+        $this->assertSame(3, $this->tree->getRoot()->getRight()->getValue());
     }
 
     public function testContainsRootAndLeftAndRightNode()
     {
-        $tree     = new Tree();
-
         $newRoot = new TreeNode(1);
-        $tree->replace(null, null, $newRoot);
+        $this->tree->replace(null, null, $newRoot);
 
         $newLeft = new TreeNode(2);
-        $tree->replace($tree->getRoot(), null, $newLeft);
+        $this->tree->replace($this->tree->getRoot(), null, $newLeft);
 
         $newRight = new TreeNode(3);
-        $tree->replace($tree->getRoot(), null, $newRight);
+        $this->tree->replace($this->tree->getRoot(), null, $newRight);
 
-        $this->assertSame(true,  $tree->contains(1));
-//        $this->assertSame(true,  $tree->contains(2));
-        $this->assertSame(true,  $tree->contains(3));
-        $this->assertSame(false, $tree->contains(4));
+        $this->assertSame(true,  $this->tree->contains(1));
+//        $this->assertSame(true,  $this->tree->contains(2));
+        $this->assertSame(true,  $this->tree->contains(3));
+        $this->assertSame(false, $this->tree->contains(4));
 
     }
 
     public function testInsertThreeNodes2And1And3()
     {
-        $tree     = new Tree();
+        $this->tree->insert(2);
+        $this->tree->insert(1);
+        $this->tree->insert(3);
 
-        $tree->insert(2);
-        $tree->insert(1);
-        $tree->insert(3);
-
-        $this->assertSame(2, $tree->getRoot()->getValue());
-        $this->assertSame(1, $tree->getRoot()->getLeft()->getValue());
-        $this->assertSame(3, $tree->getRoot()->getRight()->getValue());
+        $this->assertSame(2, $this->tree->getRoot()->getValue());
+        $this->assertSame(1, $this->tree->getRoot()->getLeft()->getValue());
+        $this->assertSame(3, $this->tree->getRoot()->getRight()->getValue());
     }
 }
